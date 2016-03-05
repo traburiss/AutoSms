c89951605a244ba68625031d3921266e2a20deb1
traburuiss li <traburuiss li>
2016-03-05 20:04:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/traburiss/autosms/MainMenu/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.traburiss.autosms.MainMenu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author traburiss
+ * @date 2016/3/5
+ * @info ${PACKAGE_NAME}.AutoSms
+ * @desc 主类，设置新的自动信息和查阅过去发送的信息
+ */
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
