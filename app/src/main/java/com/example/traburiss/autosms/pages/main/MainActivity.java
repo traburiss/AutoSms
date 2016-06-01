39111a6aa867c39ede0612568cf7a3080f658dfe
traburiss <marlycool@qq.com>
2016-06-01 09:25:40 +0800

    测试一下

--- app/src/main/java/com/example/traburiss/autosms/pages/main/MainActivity.java
+++ app/src/main/java/com/example/traburiss/autosms/pages/main/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.traburiss.autosms.R;
+
 /**
  * @author traburiss
  * @date 2016/3/5
@@ -15,7 +17,6 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        setContentView(R.layout.activity_main);
     }
 }
